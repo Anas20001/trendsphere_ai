2b5adea1b97702a3f06780ece97c0815e73a49d2
= <anasmohemmed21@yahoo.com>
2025-01-05 00:49:47 +0000

    Cleanup: Remove obsolete files and update modified configurations

--- analytics/models/staging/stg_sales.sql
+++ analytics/models/staging/stg_sales.sql
@@ -1,24 +1,21 @@
 WITH source AS (
     SELECT * FROM {{ source('raw', 'sales') }}
-),
-
-staged AS (
-    SELECT
-        id as sale_id,
-        branch_id,
-        customer_id,
-        date_id,
-        order_id,
-        total,
-        net_with_tax,
-        total_excluding_tax,
-        net,
-        net_quantity,
-        cost,
-        profit,
-        created_at,
-        updated_at
-    FROM source
 )
 
-SELECT * FROM staged 
+SELECT
+    id AS sale_id,
+    branch_id,
+    customer_id,
+    date_id,
+    order_id,
+    total,
+    total_percentage,
+    net_with_tax,
+    visitors_count,
+    discount_amount,
+    return_amount,
+    cancellation_amount,
+    profit,
+    created_at,
+    updated_at
+FROM source 
